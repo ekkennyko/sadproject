876063fe5ef22896902995fdd422b5d3e26dc946
ekkennyko <dhrhrhjdjfh@gmail.com>
2020-04-10 17:23:46 +0900

    vse ochen' ploho

--- CompetitionProject/Program.cs
+++ CompetitionProject/Program.cs
@@ -18,6 +18,7 @@ namespace CompetitionProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
         }
     }
