88e65209839e691d587e90fe588343cbe111ae47
Tendo96 <sawakanaev@gmail.com>
2020-05-14 18:03:26 +0900

    14.05.2020 KanaevAV InputResult dodelan

--- CompetitionProject/FormResult.Designer.cs
+++ CompetitionProject/FormResult.Designer.cs
@@ -29,16 +29,17 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormResult));
             this.dataResults = new System.Windows.Forms.DataGridView();
-            this.inputResult = new System.Windows.Forms.Button();
-            this.outReport = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.participantBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.personIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.middleNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Place = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.participantBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.inputResult = new System.Windows.Forms.Button();
+            this.outReport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataResults)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.participantBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -59,40 +60,6 @@
             this.dataResults.RowTemplate.Height = 24;
             this.dataResults.Size = new System.Drawing.Size(443, 228);
             this.dataResults.TabIndex = 0;
-            // 
-            // inputResult
-            // 
-            this.inputResult.Location = new System.Drawing.Point(487, 59);
-            this.inputResult.Name = "inputResult";
-            this.inputResult.Size = new System.Drawing.Size(143, 45);
-            this.inputResult.TabIndex = 1;
-            this.inputResult.Text = "Внести результаты";
-            this.inputResult.UseVisualStyleBackColor = true;
-            this.inputResult.Click += new System.EventHandler(this.inputResult_Click);
-            // 
-            // outReport
-            // 
-            this.outReport.Location = new System.Drawing.Point(487, 140);
-            this.outReport.Name = "outReport";
-            this.outReport.Size = new System.Drawing.Size(143, 45);
-            this.outReport.TabIndex = 2;
-            this.outReport.Text = "Вывести отчет";
-            this.outReport.UseVisualStyleBackColor = true;
-            this.outReport.Click += new System.EventHandler(this.outReport_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Название соревнования";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // participantBindingSource
-            // 
-            this.participantBindingSource.DataSource = typeof(CompetitionClasses.Participant);
             // 
             // personIdDataGridViewTextBoxColumn
             // 
@@ -124,6 +91,41 @@
             this.Place.HeaderText = "Место";
             this.Place.Name = "Place";
             // 
+            // participantBindingSource
+            // 
+            this.participantBindingSource.DataSource = typeof(CompetitionClasses.Participant);
+            this.participantBindingSource.CurrentChanged += new System.EventHandler(this.participantBindingSource_CurrentChanged);
+            // 
+            // inputResult
+            // 
+            this.inputResult.Location = new System.Drawing.Point(487, 59);
+            this.inputResult.Name = "inputResult";
+            this.inputResult.Size = new System.Drawing.Size(143, 45);
+            this.inputResult.TabIndex = 1;
+            this.inputResult.Text = "Внести результаты";
+            this.inputResult.UseVisualStyleBackColor = true;
+            this.inputResult.Click += new System.EventHandler(this.inputResult_Click);
+            // 
+            // outReport
+            // 
+            this.outReport.Location = new System.Drawing.Point(487, 140);
+            this.outReport.Name = "outReport";
+            this.outReport.Size = new System.Drawing.Size(143, 45);
+            this.outReport.TabIndex = 2;
+            this.outReport.Text = "Вывести отчет";
+            this.outReport.UseVisualStyleBackColor = true;
+            this.outReport.Click += new System.EventHandler(this.outReport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Название соревнования";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // FormResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -133,6 +135,7 @@
             this.Controls.Add(this.outReport);
             this.Controls.Add(this.inputResult);
             this.Controls.Add(this.dataResults);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormResult";
             this.Text = "Результаты";
             ((System.ComponentModel.ISupportInitialize)(this.dataResults)).EndInit();
